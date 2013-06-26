c605f7c27f93ee5637e15ff0880509149de2cb82
yuanzhe.guo <yuanzhe.guo@renren-inc.com>
2013-06-26 15:05:46 +0800

    初步完成主页

--- src/com/aituidao/android/listadapter/BookListAdapter.java
+++ src/com/aituidao/android/listadapter/BookListAdapter.java
@@ -6,7 +6,6 @@ import com.aituidao.android.R;
 import com.aituidao.android.data.Book;
 
 import android.content.Context;
-import android.content.res.AssetManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -47,6 +46,8 @@ public class BookListAdapter extends BaseAdapter {
 		private TextView mAuthorTv;
 		private TextView mIntroTv;
 		private TextView mPushCountTv;
+		private ImageView mHeadHandleIv;
+		private ImageView mTailHandleIv;
 	}
 	
 	@Override
@@ -55,17 +56,31 @@ public class BookListAdapter extends BaseAdapter {
 			convertView = mLayoutInflater.inflate(R.layout.book_list_adapter_item, null);
 			
 			ViewHolder holder = new ViewHolder();
-			holder.mCoverIv = (ImageView) convertView.findViewById(R.id.item_book_cover_iv);
-			holder.mTitleTv = (TextView) convertView.findViewById(R.id.item_book_title_tv);
-			holder.mAuthorTv = (TextView) convertView.findViewById(R.id.item_book_author_tv);
-			holder.mIntroTv = (TextView) convertView.findViewById(R.id.item_book_intro_tv);
-			holder.mPushCountTv = (TextView) convertView.findViewById(R.id.item_book_push_count_tv);
+			holder.mCoverIv = (ImageView) convertView.findViewById(R.id.item_cover_iv);
+			holder.mTitleTv = (TextView) convertView.findViewById(R.id.item_title_tv);
+			holder.mAuthorTv = (TextView) convertView.findViewById(R.id.item_author_tv);
+			holder.mIntroTv = (TextView) convertView.findViewById(R.id.item_intro_tv);
+			holder.mPushCountTv = (TextView) convertView.findViewById(R.id.item_push_count_tv);
+			holder.mHeadHandleIv = (ImageView) convertView.findViewById(R.id.item_head_handle_iv);
+			holder.mTailHandleIv = (ImageView) convertView.findViewById(R.id.item_tail_handle_iv);
 			
 			convertView.setTag(holder);
 		}
 		
 		ViewHolder holder = (ViewHolder) convertView.getTag();
 		Book book = mList.get(position);
+		
+		if (position == 0) {
+			holder.mHeadHandleIv.setImageResource(R.drawable.book_item_head_handle_for_first);
+		} else {
+			holder.mHeadHandleIv.setImageResource(R.drawable.book_item_head_handle);
+		}
+		
+		if (position == mList.size() - 1) {
+			holder.mTailHandleIv.setVisibility(View.INVISIBLE);
+		} else {
+			holder.mTailHandleIv.setVisibility(View.VISIBLE);
+		}
 		
 		// TODO 这里是url，要下载，现在只是临时代码
 		holder.mCoverIv.setImageResource(book.mCoverUrl);
