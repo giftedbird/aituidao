78f1c95e4b8648a2dbfbc1b64e12ec2f851be158
yuanzhe.guo <yuanzhe.guo@renren-inc.com>
2013-06-28 16:27:57 +0800

    json试用

--- src/com/aituidao/android/data/BookListResponse.java
+++ src/com/aituidao/android/data/BookListResponse.java
@@ -3,33 +3,9 @@ package com.aituidao.android.data;
 import java.util.List;
 
 public class BookListResponse {
-	public static final int OK = 0;
+	public static final int OK = 1;
 	
 	public int status;
 	public long nextPageNum;
 	public List<Book> bookList;
-	
-	public int getStatus() {
-		return status;
-	}
-	
-	public void setStatus(int status) {
-		this.status = status;
-	}
-	
-	public long getNextPageNum() {
-		return nextPageNum;
-	}
-	
-	public void setNextPageNum(long nextPageNum) {
-		this.nextPageNum = nextPageNum;
-	}
-	
-	public List<Book> getBookList() {
-		return bookList;
-	}
-	
-	public void setBookList(List<Book> bookList) {
-		this.bookList = bookList;
-	}
 }
--- src/com/aituidao/android/helper/BookListHelper.java
+++ src/com/aituidao/android/helper/BookListHelper.java
@@ -7,6 +7,7 @@ import java.util.Random;
 import com.aituidao.android.R;
 import com.aituidao.android.data.Book;
 import com.aituidao.android.data.BookListResponse;
+import com.alibaba.fastjson.JSON;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
@@ -144,45 +145,10 @@ public class BookListHelper {
 			} catch (InterruptedException e) {
 			}
 			
-			List<Book> list = new ArrayList<Book>();
-			Random r = new Random(System.currentTimeMillis());
-			for (int i = 0; i < 20; i++) {
-				Book book = new Book();
-				switch (r.nextInt(3)) {
-				case 0:
-					book.author = "【港】许中约";
-					book.title = "中国近代史";
-					book.pushCount = 37;
-					book.intro = "《中国近代史》自1970年面世后五次修订，销售数十万册，为欧美及东南亚等地中国近代史研究的权威著作及最畅销的学术教科书，是一本极具深远影响的经典作品。";
-					book.coverUrl = R.drawable.temp_1;
-					break;
-					
-				case 1:
-					book.author = "史玉柱口述 优米网编著";
-					book.title = "史玉柱自述";
-					book.pushCount = 5;
-					book.intro = "史玉柱迄今为止唯一公开著作。亲口讲述24年创业历程与营销心得。中国商业思想史里程碑之作！24年跌宕起伏，功成身退，史玉柱向您娓娓道来，历经时间沉淀的商业智慧和人生感悟。在书中，史玉柱毫无保留地回顾了创业以来的经历和各阶段的思考。全书没有深奥的理论，铅华洗尽、朴实无华，往往在轻描淡写之间，一语道破营销的本质。关于产品开发、营销传播、广告投放、团队管理、创业投资等，史玉柱都做了独特而富有洞见的思考，在启迪读者的同时，也为中国商界留下了一份弥足珍贵的商业思想记录。随便翻翻就有收获，反复体会更觉深刻。";
-					book.coverUrl = R.drawable.temp_2;
-					break;
-					
-				case 2:
-					book.author = "【日】松本行弘 ";
-					book.title = "代码的未来";
-					book.pushCount = 90;
-					book.intro = "《代码的未来》是Ruby之父松本行弘的又一力作。作者对云计算、大数据时代下的各种编程语言以及相关技术进行了剖析，并对编程语言的未来发展趋势做出预测，。";
-					book.coverUrl = R.drawable.temp_3;
-					break;
-				}
-				
-				list.add(book);
-			}
-			
-			BookListResponse response = new BookListResponse();
-			response.status = BookListResponse.OK;
-			response.nextPageNum = mPageNo + 1 > 5 ? -1 : mPageNo + 1;
-			response.bookList = list;
+			String responseStr = "{\"bookList\":[{\"author\":\"【日】松本行弘 \",\"coverUrl\":2130837531,\"id\":9748,\"intro\":\"超级书\",\"pushCount\":90,\"title\":\"代码的未来\"},{\"author\":\"【日】松本行弘 \",\"coverUrl\":2130837531,\"id\":9748,\"intro\":\"超级书\",\"pushCount\":90,\"title\":\"代码的未来\"},{\"author\":\"史玉柱口述 优米网编著\",\"coverUrl\":2130837530,\"id\":9334,\"intro\":\"牛逼书\",\"pushCount\":5,\"title\":\"史玉柱自述\"},{\"author\":\"【日】松本行弘 \",\"coverUrl\":2130837531,\"id\":9748,\"intro\":\"超级书\",\"pushCount\":90,\"title\":\"代码的未来\"},{\"author\":\"【港】许中约\",\"coverUrl\":2130837529,\"id\":9304,\"intro\":\"好书\",\"pushCount\":37,\"title\":\"中国近代史\"},{\"author\":\"【港】许中约\",\"coverUrl\":2130837529,\"id\":9304,\"intro\":\"好书\",\"pushCount\":37,\"title\":\"中国近代史\"},{\"author\":\"【日】松本行弘 \",\"coverUrl\":2130837531,\"id\":9748,\"intro\":\"超级书\",\"pushCount\":90,\"title\":\"代码的未来\"},{\"author\":\"史玉柱口述 优米网编著\",\"coverUrl\":2130837530,\"id\":9334,\"intro\":\"牛逼书\",\"pushCount\":5,\"title\":\"史玉柱自述\"},{\"author\":\"【日】松本行弘 \",\"coverUrl\":2130837531,\"id\":9748,\"intro\":\"超级书\",\"pushCount\":90,\"title\":\"代码的未来\"},{\"author\":\"【港】许中约\",\"coverUrl\":2130837529,\"id\":9304,\"intro\":\"好书\",\"pushCount\":37,\"title\":\"中国近代史\"},{\"author\":\"【港】许中约\",\"coverUrl\":2130837529,\"id\":9304,\"intro\":\"好书\",\"pushCount\":37,\"title\":\"中国近代史\"},{\"author\":\"史玉柱口述 优米网编著\",\"coverUrl\":2130837530,\"id\":9334,\"intro\":\"牛逼书\",\"pushCount\":5,\"title\":\"史玉柱自述\"},{\"author\":\"【港】许中约\",\"coverUrl\":2130837529,\"id\":9304,\"intro\":\"好书\",\"pushCount\":37,\"title\":\"中国近代史\"},{\"author\":\"【日】松本行弘 \",\"coverUrl\":2130837531,\"id\":9748,\"intro\":\"超级书\",\"pushCount\":90,\"title\":\"代码的未来\"},{\"author\":\"史玉柱口述 优米网编著\",\"coverUrl\":2130837530,\"id\":9334,\"intro\":\"牛逼书\",\"pushCount\":5,\"title\":\"史玉柱自述\"},{\"author\":\"史玉柱口述 优米网编著\",\"coverUrl\":2130837530,\"id\":9334,\"intro\":\"牛逼书\",\"pushCount\":5,\"title\":\"史玉柱自述\"},{\"author\":\"史玉柱口述 优米网编著\",\"coverUrl\":2130837530,\"id\":9334,\"intro\":\"牛逼书\",\"pushCount\":5,\"title\":\"史玉柱自述\"},{\"author\":\"【港】许中约\",\"coverUrl\":2130837529,\"id\":9304,\"intro\":\"好书\",\"pushCount\":37,\"title\":\"中国近代史\"},{\"author\":\"【港】许中约\",\"coverUrl\":2130837529,\"id\":9304,\"intro\":\"好书\",\"pushCount\":37,\"title\":\"中国近代史\"},{\"author\":\"史玉柱口述 优米网编著\",\"coverUrl\":2130837530,\"id\":9334,\"intro\":\"牛逼书\",\"pushCount\":5,\"title\":\"史玉柱自述\"}],\"nextPageNum\":1,\"status\":1}";
 			// TODO demo代码结束
 			
+			BookListResponse response = JSON.parseObject(responseStr, BookListResponse.class);
 			if (mPageNo <= 0) {
 				if (response.status == BookListResponse.OK) {
 					mHandler.sendMessage(mHandler.obtainMessage(REFRESH_BOOK_LIST_SUCCESS,
